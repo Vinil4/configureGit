32874bb798ca3ce4e205e35b04ee3cb28a0decca
Gustavo <gustavo.oliveira3@academico.ufpb.br>
2026-03-07 13:40:06 -0300

    chrome instalation hotfix

--- setup-linux.sh
+++ setup-linux.sh
@@ -110,7 +110,7 @@ echo "Instalando o Google-Chrome...\n\n"
 
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 
-sudo apt install ./google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
 rm -rf google-chrome-stable_current_amd64.deb
 
 #==================================================
